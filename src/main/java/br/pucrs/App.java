84b340f5673cec20c22351d35e0e89590554186c
Felipe Elsner <66232795+Elsner2002@users.noreply.github.com>
2023-03-14 18:20:40 -0300

    ex 3 pronto

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -1,13 +1,37 @@
 package br.pucrs;
+import java.util.Random;
+import java.util.regex.MatchResult;
+import java.lang.Math;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class App {
+  public static void main(String[] args) {
+    long start = System.currentTimeMillis();
+    int tam=2048;
+    long[] vet32 = randVetor(tam);
+    maxVal2(vet32, 0, tam-1);
+    long finish = System.currentTimeMillis();
+    long total = finish - start;
+    System.out.println("tam 32 leva "+total+"ms");
+  }
+
+  public static long[] randVetor(int tam){
+    long[] vet = new long [tam];
+    int bound=tam*5;
+    Random ran=new Random();
+    for(int i=0; i<tam-1;i++){
+      vet[i]= ran.nextInt(bound);
     }
+    return vet;
+  }
+
+  public static long maxVal2(long A[], int init, int end) {  
+    if (end - init <= 1)
+        return Math.max(A[init], A[end]);  
+    else {
+          int m = (init + end)/2;
+          long v1 = maxVal2(A,init,m);   
+          long v2 = maxVal2(A,m+1,end);  
+          return Math.max(v1,v2);
+         }
+}
 }
