5b4ace7b6cb0c03de31058771d73afb76b27ef48
BERNARDO BARZOTTO ZOMER <Bernardo.Barzotto@facin.portoalegre.pucrsnet.br>
2023-03-16 18:59:53 -0300

    feat: implement karatsuba and iteration counter

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -12,11 +12,15 @@ public class App
 	 * Should not be read between resetTimer and stopTimer.
 	 */
 	private static long timer;
+	private	static long iter_maxVal1;
+	private	static long iter_maxVal2;
+	private	static long iter_mergeSort;
+	private	static long iter_karatsuba;
 
 	public static void main(String[] args) {
-		App.benchmark(32);
-		App.benchmark(2048);
-		App.benchmark(1_048_576);
+		App.benchmark(32, 4);
+		App.benchmark(2048, 16);
+		App.benchmark(1_048_576, 64);
 	}
 
 	public static List<Integer> randomList(int size){
@@ -35,6 +39,8 @@ public class App
 		Integer max = list.get(0);
 
 		for (Integer x : list) {
+			App.iter_maxVal1++;
+
 			if (x > max) {
 				max = x;
 			}
@@ -46,6 +52,8 @@ public class App
 	public static Integer maxVal2(
 		List<Integer> list, Integer start, Integer end
 	) {
+		App.iter_maxVal2++;
+
 		if (end - start <= 1) {
 			return Math.max(list.get(start), list.get(end));
 		} else {
@@ -64,6 +72,8 @@ public class App
 	 * @return a new list with the elements from list in ascending order
 	*/
 	public static <T extends Comparable<T>> List<T> mergeSort(List<T> list) {
+		App.iter_mergeSort++;
+
 		if (list.size() < 2) {
 			return list;
 		}
@@ -102,27 +112,56 @@ public class App
 		return res;
 	}
 
-	private static void benchmark(int size) {
-		System.out.println("Size: " + size + "\n");
-		List<Integer> list = randomList(size);
+	public static long karatsuba(long x, long y, int size) {
+		App.iter_karatsuba++;
+
+		if (size == 1) {
+			return x * y;
+		} else {
+			int m = (int) Math.ceil(size / 2);
+			long a = (long) (Math.floor(x / Math.pow(2, m)));
+			long b = (long) (x % Math.pow(2, m));
+			long c = (long) (Math.floor(x / Math.pow(2, m)));
+			long d = (long) (y % Math.pow(2, m));
+			long e = karatsuba(a, c, m);
+			long f = karatsuba(b, d, m);
+			long g = karatsuba(b, c, m);
+			long h = karatsuba(a, d, m);
+
+			return (long) (Math.pow(2, 2 * m)) * e
+				+ (long) (Math.pow(2, m)) * (g + h) + f;
+		}
+	}
+
+	private static void benchmark(int list_size, int int_size) {
+		App.resetIterations();
+		System.out.println("Array Size: " + list_size);
+		System.out.println("Integer Size: " + int_size + "\n");
+		List<Integer> list = App.randomList(list_size);
 
 		// Benchmarks max without divide and conquer.
 		App.resetTimer();
-		maxVal1(list);
+		App.maxVal1(list);
 		double time = App.stopTimer();
-		System.out.println("max (no divide and conquer): " + time + " ms");
+		System.out.println("max (no divide and conquer): " + App.iter_maxVal1 + " iters (" + time + " ms)");
 
 		// Benchmarks max with divide and conquer.
 		App.resetTimer();
-		maxVal2(list, 0, size - 1);
+		App.maxVal2(list, 0, list_size - 1);
 		time = App.stopTimer();
-		System.out.println("max (with divide and conquer): " + time + " ms");
+		System.out.println("max (with divide and conquer): " + App.iter_maxVal2 + " iters (" + time + " ms)");
 
 		// Benchmarks merge sort.
 		App.resetTimer();
-		mergeSort(list);
+		App.mergeSort(list);
 		time = App.stopTimer();
-		System.out.println("merge sort: " + time + " ms");
+		System.out.println("merge sort: " + App.iter_mergeSort + " iters (" + time + " ms)");
+
+		// Benchmarks the Karatsuba algorithm.
+		App.resetTimer();
+		App.karatsuba(Long.MAX_VALUE, Long.MAX_VALUE, int_size);
+		time = App.stopTimer();
+		System.out.println("karatsuba: " + App.iter_karatsuba + " iters (" + time + " ms)");
 
 		System.out.println("\n");
 	}
@@ -144,5 +183,12 @@ public class App
 	private static double stopTimer() {
 		App.timer = System.nanoTime() - App.timer;
 		return App.timer / 1e6;
+	}
+
+	private static void resetIterations() {
+		App.iter_maxVal1 = 0;
+		App.iter_maxVal2 = 0;
+		App.iter_mergeSort = 0;
+		App.iter_karatsuba = 0;
 	}
 }
