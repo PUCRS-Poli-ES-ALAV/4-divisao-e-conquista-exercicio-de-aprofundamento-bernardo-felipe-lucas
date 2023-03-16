92e7c78ff17ae5f40ece8ec081ec0deaedfbed64
Bernardozomer <bernardobarzottoz@gmail.com>
2023-03-16 11:10:36 -0300

    style!: apply code style

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -1,59 +1,71 @@
 package br.pucrs;
-import java.util.Random;
 
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class App
 {
-    public static void main(String[] args) {
-        int size = 1048576;
-        long[] vet = randVetor(size);
+	public static void main(String[] args) {
+		int size = 1048576;
+		long[] vec = randomVector(size);
 
-        long start = System.currentTimeMillis();
-        maxVal1(vet, size);
-        long finish = System.currentTimeMillis();
-        long total = finish - start;
-        System.out.println("O vetor de tamanho "+ size +" sem divisao e conquista leva "+total+" de milisegundos para executar\n");
+		long start = System.currentTimeMillis();
+		maxVal1(vec, size);
+		long finish = System.currentTimeMillis();
+		long total = finish - start;
 
+		System.out.println(
+			"O vetor de tamanho " + size + " sem divisao e conquista leva "
+			+ total + " de milisegundos para executar\n"
+		);
 
-        long start2 = System.currentTimeMillis();
-        maxVal2(vet, 0, size-1);
-        long finish2 = System.currentTimeMillis();
-        long total2 = finish2 - start2;
-        System.out.println("O vetor de tamanho "+size+" com divisao e conquista leva "+total2+" de milisegundos para executar");
-    }
+		start = System.currentTimeMillis();
+		maxVal2(vec, 0, size - 1);
+		finish = System.currentTimeMillis();
+		total = finish - start;
 
-      public static long[] randVetor(int tam){
-        long[] vet = new long [tam];
-        int bound=tam*5;
-        Random ran=new Random();
-        for(int i=0; i<tam-1;i++){
-          vet[i]= ran.nextInt(bound);
-        }
-        return vet;
-      }
+		System.out.println(
+			"O vetor de tamanho " + size + " com divisao e conquista leva "
+			+ total + " milisegundos para executar"
+		);
+	}
 
-    public static long maxVal1(long A[], int n) {
-        long max = A[0];
-        for (int i = 1; i < n; i++) {
-            if( A[i] > max )
-                max = A[i];
-        }
-            return max;
-    }
+	public static long[] randomVector(int size){
+		long[] vec = new long[size];
+		int bound = size * 5;
+		Random random = new Random();
 
-      public static long maxVal2(long A[], int init, int end) {
-        if (end - init <= 1)
-            return Math.max(A[init], A[end]);
-        else {
-              int m = (init + end)/2;
-              long v1 = maxVal2(A,init,m);
-              long v2 = maxVal2(A,m+1,end);
-              return Math.max(v1,v2);
-             }
-    }
+		for(int i = 0; i < size - 1; i++){
+			vec[i] = random.nextInt(bound);
+		}
+
+		return vec;
+	}
+
+	public static long maxVal1(long vec[], int n) {
+		long max = vec[0];
+
+		for (int i = 1; i < n; i++) {
+			if( vec[i] > max ) {
+				max = vec[i];
+			}
+		}
+
+		return max;
+	}
+
+	public static long maxVal2(long vec[], int start, int end) {
+		if (end - start <= 1) {
+			return Math.max(vec[start], vec[end]);
+		} else {
+			int m = (start + end) / 2;
+			long v1 = maxVal2(vec, start, m);
+			long v2 = maxVal2(vec, m + 1, end);
+			return Math.max(v1, v2);
+		}
+	}
 
 	/**
 	 * Sorts a list of comparable elements using the merge sort algorithm.
@@ -61,14 +73,14 @@ public class App
 	 * @param <T> the type of elements in the list
 	 * @param v the list of elements to sort
 	 * @return a new list with the elements from v, sorted in ascending order
-	 */
-	public static <T extends Comparable<T>> List<T> merge_sort(List<T> v) {
+	*/
+	public static <T extends Comparable<T>> List<T> mergeSort(List<T> v) {
 		if (v.size() < 2) {
 			return v;
 		}
 
-		List<T> a = merge_sort(v.subList(0, v.size() / 2));
-		List<T> b = merge_sort(v.subList(v.size() / 2, v.size()));
+		List<T> a = mergeSort(v.subList(0, v.size() / 2));
+		List<T> b = mergeSort(v.subList(v.size() / 2, v.size()));
 
 		// Merge both arrays.
 		List<T> res = new ArrayList<>(v.size());
